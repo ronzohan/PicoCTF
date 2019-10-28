5f981206bc0157b9cedd4b2cbcec43081d847778
Ron Daryl Magno <ron.zohan@gmail.com>
2019-10-29 00:32:42 +0800

    Added encoding of strings

--- securelogin/bit_flip_attack.py
+++ securelogin/bit_flip_attack.py
@@ -4,6 +4,10 @@ from base64 import b64encode
 from Crypto import Random
 from Crypto.Cipher import AES
 
+BLOCK_SIZE = 16  # Bytes
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE).encode()
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
 def decode_text(enc):
     decoded_text = b64decode(enc)
     iv = decoded_text[:16]
@@ -28,19 +32,32 @@ class AESCipher:
     def encrypt(self, raw):
         raw = pad(raw)
         iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        cipher = AES.new(self.key.encode(), AES.MODE_CBC, iv)
         return b64encode(iv + cipher.encrypt(raw))
 
     def decrypt(self, enc):
         enc = b64decode(enc)
         iv = enc[:16]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return unpad(cipher.decrypt(enc[16:])).decode('utf8')
+        cipher = AES.new(self.key.encode(), AES.MODE_CBC, iv)
+        dec = cipher.decrypt(enc[16:])
+        print('Decrypted cipher', dec.hex())
+        return unpad(dec).decode('utf8')
 
 if __name__ == '__main__':
     cookie = 'Piw96Gvx5Jz01V7iuMXSwWDkcRYB1vIVzj5atcBhkuqsQTkDgjoYYj7UWvAR9L6F/v0rCYLU5N1tq6zAU7XnObn1+2AEjRv8kb1PD9onqqA='
+    expected_string = "{'username': 'aaaaa', 'password': '', 'admin': 0}"
 
-    iv, cipher = decode_text(cookie)
+    aes_cipher = AESCipher('123456')
+    enc = aes_cipher.encrypt(expected_string.encode())
+    dec = aes_cipher.decrypt(enc)
+
+    iv, cipher = decode_text(enc)
 
     print('IV', iv.hex())
     print('Cipher', cipher.hex())
+    print('Decrepted text', dec)
+
+
+
+
+
